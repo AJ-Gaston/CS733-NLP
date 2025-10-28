b9c6f524d236937a71339865e3c32af3110387e5
Alexji Gaston <agast002@odu.edu>
2025-10-28 11:42:40 -0400

    'Working on assignment 4'

---
+++ Assignment4/feedforward_neural_network.py
@@ -0,0 +1,7 @@
+class feedforward_nn:
+    def __init__(self, learning_rate = 0.001, epoch =30):
+        
+        self.learning_rate = learning_rate
+        self.epochs = epoch
+        
+        #Training variables
---
+++ Assignment4/main.py
@@ -0,0 +1,11 @@
+import csv
+import re
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import feedforward_neural_network
+
+def main():
+
+if __name__ == '__main__':
+    main()
