914f30ecece208a1251f8239deb93b333957a924
Alexji Gaston <agast002@odu.edu>
2025-10-28 14:26:39 -0400

    'Working on Assignment4'

--- Assignment4/main.py
+++ Assignment4/main.py
@@ -5,7 +5,30 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import feedforward_neural_network
 
+def preprocessing(text):
+    """
+    Preprocess the text in dataset (lower the words, remove stopwords, etc.)
+    Args:
+        text (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    preprocess
+    return preprocessed_text
+
+def perplexity():
+    """
+    Calculate the perplexity of a language model
+
+    Returns:
+        model_perplexity (float): an indicator of the model's perplexity
+    """
+    model_perplexity = 0
+    return model_perplexity
+
 def main():
+    
 
 if __name__ == '__main__':
     main()
